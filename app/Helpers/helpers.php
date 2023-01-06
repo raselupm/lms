666cb744f4340caa8cd66449bef2273c8dedd83d
mostafizmohfw <mostafizmohfw@gmail.com>
2023-01-07 00:31:15 +0600

    Fixing Helper Function Redirect issue, Course Create with Curriculum and User Index Page

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Auth;
 function permission_check($permission) {
     if(!Auth::user()->hasPermissionTo($permission)) {
         flash()->addWarning('You are not authorized to access this page');
-        return redirect()->back();
+        return redirect()->route('dashboard')->send();
     }
 }
---
+++ app/Http/Livewire/UserEdit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class UserEdit extends Component
+{
+    public function render()
+    {
+        return view('livewire.user-edit');
+    }
+}
